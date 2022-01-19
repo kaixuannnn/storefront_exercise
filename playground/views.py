7ff24d6c45834113ffc6d8090a0de7a92ef0495a
kaixuan <kaixuant@yahoo.com>
2022-01-19 14:37:06 +0800

    Or on filter object

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
-
-
-
 def say_hello(request):
-    queryset = Product.objects.filter(unit_price__range=(20,30))
-    #return bolean
+    # Products: inventory < 10 OR price <20 
+    queryset = Product.objects.filter(Q(inventory__lt=0)|~Q(unit_price__lt=20))
+    #~ symbolise Not
 
     return render(request, 'hello.html', {'name': 'Mosh', 'products':list(queryset)})
