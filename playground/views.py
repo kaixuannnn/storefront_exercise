a74de72cc530147bf77a12f204d6af61b1db6f4a
kaixuan <kaixuant@yahoo.com>
2022-01-19 14:50:04 +0800

    F reference query method

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 def say_hello(request):
-    # Products: inventory < 10 OR price <20 
-    queryset = Product.objects.filter(Q(inventory__lt=0)|~Q(unit_price__lt=20))
+    # Products: inventory = price
+    queryset = Product.objects.filter(inventory=F('collection__id'))
     #~ symbolise Not
 
     return render(request, 'hello.html', {'name': 'Mosh', 'products':list(queryset)})
