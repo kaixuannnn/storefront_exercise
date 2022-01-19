f90f300a77f4944ded4ca082f605b05b24e6c7b3
kaixuan <kaixuant@yahoo.com>
2022-01-19 15:09:50 +0800

    select fields to query

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 def say_hello(request):
-    # sort the queryset
-    queryset = Product.objects.order_by('unit_price', '-title').reverse()
-    #order with unit price in ascending order and titkle in descending order
+    #distinct to remove duplicate item
+    productIds =  OrderItem.objects.values('product__id').distinct()
+    queryset= Product.objects.filter(id__in=productIds).order_by('title')
 
     return render(request, 'hello.html', {'name': 'Mosh', 'products':list(queryset)})
