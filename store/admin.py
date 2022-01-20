434a074d191443eefc205250adb7a4f2c80488e1
kaixuan <kaixuant@yahoo.com>
2022-01-20 12:41:26 +0800

    add computed columns

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,15 @@ from . import models
 
 @admin.register(models.Product)
 class ProdictAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'LOW'
+        return 'OK'
 
 
 admin.site.register(models.Collection)
