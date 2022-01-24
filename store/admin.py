a910c6e573717540ae11943b3a861627e60c434b
kaixuan <kaixuant@yahoo.com>
2022-01-24 10:44:52 +0800

    building collections API

--- store/admin.py
+++ store/admin.py
@@ -72,7 +72,7 @@ class CollectionAdmin(admin.ModelAdmin):
         return format_html('<a href="{}">{}</>',url,collection.products_count)
 
     def get_queryset(self, request: HttpRequest) :
-        return super().get_queryset(request).annotate(products_count=Count('product'))
+        return super().get_queryset(request).annotate(products_count=Count('products'))
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
--- store/serializer.py
+++ store/serializer.py
@@ -7,9 +7,10 @@ from store.models import Product, Collection
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model =  Collection
-        fields = ['id', 'title']
+        fields = ['id', 'title', 'products_count']
     # id = serializers.IntegerField()
     # title = serializers.CharField(max_length = 255)
+    products_count = serializers.IntegerField()
 
 #serializer -  can use it to convert a product object to a Python dictionary
 class ProductSerializer(serializers.ModelSerializer):
