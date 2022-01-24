11fc98d7cd3b2cd4444df8ed8481dd444b7acdd2
kaixuan <kaixuant@yahoo.com>
2022-01-24 17:55:53 +0800

    searching & ordering

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
@@ -12,8 +13,10 @@ from .serializer import CollectionSerializer, ProductSerializer, ReviewSerialize
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['title','description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
