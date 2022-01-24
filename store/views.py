60a0123dc80afb670e0d02dff908b409546ba4a4
kaixuan <kaixuant@yahoo.com>
2022-01-24 16:47:18 +0800

    add query filter collection id

--- store/views.py
+++ store/views.py
@@ -1,14 +1,21 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet 
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from .models import Collection, OrderItem, Product, Review
 from .serializer import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get('collection_id')
+        if collection_id is not None:
+            queryset =  queryset.filter(collection_id=collection_id)
+
+        return queryset
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
@@ -42,7 +49,7 @@ class CollectionViewSet(ModelViewSet):
 
 
 class ReviewViewSet(ModelViewSet):
-    
+
     serializer_class = ReviewSerializer
 
     def get_queryset(self):
