4c6b4b775e9a4e7ab3436816b00095b0ce3d1bf1
kaixuan <kaixuant@yahoo.com>
2022-01-25 09:38:38 +0800

    pagination

--- store/views.py
+++ store/views.py
@@ -2,19 +2,21 @@ from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-
-from store.filters import ProductFilter
+from .filters import ProductFilter
 from .models import Collection, OrderItem, Product, Review
 from .serializer import CollectionSerializer, ProductSerializer, ReviewSerializer
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = PageNumberPagination
     search_fields = ['title','description']
     ordering_fields = ['unit_price', 'last_update']
 
